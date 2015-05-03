607b0528beb6eefaadc88ea04e7c978c891e390f
林太伟 <davidleen29@gmail.com>
2015-05-04 00:01:49 +0800

    Initial commit

---
+++ Server/src/main/java/com/giants/hd/server/TestServer.java
@@ -0,0 +1,14 @@
+package com.giants.hd.server;
+
+import com.giants3.hd.utils.TestUtils;
+
+/**
+ * Created by davidleen29 on 2015/5/3.
+ */
+public class TestServer {
+
+    public static  final  void test()
+    {
+        TestUtils.Test();
+    }
+}
---
+++ Utils/src/main/java/com/giants3/hd/utils/TestUtils.java
@@ -0,0 +1,13 @@
+package com.giants3.hd.utils;
+
+/**
+ * Created by davidleen29 on 2015/5/3.
+ */
+public class TestUtils {
+
+
+    public static final void Test()
+    {
+        System.out.println("start!");
+    }
+}
