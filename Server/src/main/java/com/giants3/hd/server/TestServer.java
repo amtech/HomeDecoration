fa5bc94da3f761522816b60d7598b1b918f2dd34
davidleen <davidleen29@gmailc.om>
2015-05-04 14:37:13 +0800

    add gradle dependency  spring

--- Server/src/main/java/com/giants3/hd/server/TestServer.java
+++ Server/src/main/java/com/giants3/hd/server/TestServer.java
@@ -1,10 +1,12 @@
 package com.giants3.hd.server;
 
 import com.giants3.hd.utils.TestUtils;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by davidleen29 on 2015/5/3.
  */
+
 public class TestServer {
 
     public static final void test()
--- Utils/src/main/java/com/giants3/hd/utils/TestUtils.java
+++ Utils/src/main/java/com/giants3/hd/utils/TestUtils.java
@@ -1,5 +1,8 @@
 package com.giants3.hd.utils;
 
+import de.greenrobot.common.io.FileUtils;
+import sun.misc.IOUtils;
+
 /**
  * Created by davidleen29 on 2015/5/3.
  */
@@ -8,6 +11,7 @@ public class TestUtils {
 
     public static final void Test()
     {
+
         System.out.println("start!");
     }
 }
