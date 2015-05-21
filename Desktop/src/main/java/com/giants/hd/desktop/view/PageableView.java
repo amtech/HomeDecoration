fa2150639448dd7665d22c354358aa43e6b80016
davidleen <davidleen29@gmailc.om>
2015-05-21 17:38:13 +0800

    update a few view

---
+++ Desktop/src/main/java/com/giants/hd/desktop/view/PageableView.java
@@ -0,0 +1,25 @@
+package com.giants.hd.desktop.view;
+
+import com.giants3.hd.utils.RemoteData;
+
+import javax.swing.*;
+
+/**
+ * Created by Administrator on 2015-05-21.
+ */
+
+public class PageableView  {
+    private JButton button1;
+    private JPanel panel1;
+
+
+    public void setData(RemoteData data) {
+    }
+
+    public void getData(RemoteData data) {
+    }
+
+    public boolean isModified(RemoteData data) {
+        return false;
+    }
+}
