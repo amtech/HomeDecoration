e741ea216867eda92eed77adea70c5c3c3a89e27
林太伟 <davidleen29@gmail.com>
2015-06-05 10:28:46 +0800

    add pack controller

---
+++ Server/src/main/java/com/giants3/hd/server/repository/PackRepository.java
@@ -0,0 +1,14 @@
+package com.giants3.hd.server.repository;
+//
+
+import com.giants3.hd.utils.entity.Pack;
+import com.giants3.hd.utils.entity.PackMaterialClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+* 包装类型
+ *
+*/
+public interface PackRepository extends JpaRepository<Pack,Long> {
+
+}
